a9c4bfe443d0a3e6e909080ca15fa8c09ae37af0
SeonJK <tjswhdrbs14@naver.com>
2022-04-18 21:43:41 +0900

    BOJ(1008)

---
+++ BOJ(1008).kt
@@ -0,0 +1,20 @@
+package ProblemSolvings
+
+import java.util.Scanner
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)){
+    println(nextDouble() / nextDouble())
+}
+
+//fun ProblemSolvings.BOJ.main(args: Array<String>) {
+//    val inputString = readLine()
+//    val stringArray = inputString!!.split(" ")
+//
+//    var doubleArray: MutableList<Double> = mutableListOf()
+//    stringArray.forEach{
+//         doubleArray.add(it.toDouble())
+//    }
+//    val result = doubleArray[0] / doubleArray[1]
+//
+//    println(result)
+//}
