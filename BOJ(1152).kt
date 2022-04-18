fa9f89f2151b3afd8060eec30bb9a0308ebcd25e
SeonJK <tjswhdrbs14@naver.com>
2022-04-18 21:10:32 +0900

    BOJ(1152)

---
+++ BOJ(1152).kt
@@ -0,0 +1,7 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    print("${StringTokenizer(readLine(), " ").countTokens()}")
+}
