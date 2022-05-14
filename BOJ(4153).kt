867e9b63505e414b486db8846aa02d95cbe8041a
SeonJK <tjswhdrbs14@naver.com>
2022-05-14 14:59:41 +0900

    BOJ(4153)

---
+++ BOJ(4153).kt
@@ -0,0 +1,22 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    var a = nextInt()
+    var b = nextInt()
+    var c = nextInt()
+
+    while (a != 0) {
+        if ((a.times(a) == (b.times(b) + c.times(c))) ||
+            (b.times(b) == (a.times(a) + c.times(c))) ||
+            (c.times(c) == (a.times(a) + b.times(b)))
+        ) {
+            println("right")
+        } else {
+            println("wrong")
+        }
+
+        a = nextInt()
+        b = nextInt()
+        c = nextInt()
+    }
+}
