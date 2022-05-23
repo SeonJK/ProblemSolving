13532086fde3ce7d94f12c2f30f0f6679445d542
SeonJK <tjswhdrbs14@naver.com>
2022-05-23 16:52:12 +0900

    Review and Add comments

--- BOJ(15829).kt
+++ BOJ(15829).kt
@@ -3,37 +3,42 @@
 import java.util.Scanner
 
 fun main() = with(Scanner(System.`in`)){
-    val length = readLine()!!.toInt()
-    var cnt = 0
-    var sum: Long = 0
+    // 시그마를 다 하고 mod 연산을 하지 않고
+    // 각각에 mod 연산을 해서 문제 해결
 
-    readLine()!!.forEach { char ->
-        var temp: Long = (char.code - 96).toLong()
-        for (i in 0 until cnt) {
-            temp *= 31
-            temp %= 1234567891
-        }
-        sum = (sum+temp) % 1234567891
-        cnt++
-    }
-
-    print(sum)
-
-
-    // (a*b)%N == { (a%N) * (b%N) } % N
+    // 방법 1.
 //    val length = readLine()!!.toInt()
-//    var go = 0
-//    var expo = 1L
+//    var cnt = 0
 //    var sum = 0L
 //
-//    readLine()!!.forEach {
-//        // 26보다 큰 소수를 제곱하는 수에 mod 1234567891
-//        expo = if (go==0) 1L else (expo*31)%1234567891
-//        // 항의 번호에 해당하는 만큼 특정한 숫자를 거듭제곱해서 곱해준 다음 더한 값에
-//        // mod 1234567891
-//        sum = (sum + (it.code - 96) * expo) % 1234567891
-//
-//        go++
+//    readLine()!!.forEach { char ->
+//        var temp: Long = (char.code - 96).toLong()
+//        // 특정 소수 거듭제곱 부분
+//        for (i in 0 until cnt) {
+//            temp *= 31
+//            temp %= 1234567891
+//        }
+//        // 시그마 부분
+//        sum = (sum+temp) % 1234567891
+//        cnt++
 //    }
-//    print(sum%1234567891)
+//
+//    print(sum)
+
+
+    // 방법 2.
+    val length = readLine()!!.toInt()
+    var go = 0
+    var expo = 1L
+    var sum = 0L
+
+    readLine()!!.forEach {
+        // 특정 소수 제곱하는 부분
+        expo = if (go==0) 1L else (expo*31)%1234567891
+        // 시그마 부분
+        sum = (sum + (it.code - 96) * expo) % 1234567891
+
+        go++
+    }
+    print(sum)
 }
