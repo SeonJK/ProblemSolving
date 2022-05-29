038cdcf0b893bd05da3768f90ca355242cb2a549
SeonJK <tjswhdrbs14@naver.com>
2022-05-29 14:36:52 +0900

    BOJ(2869)

---
+++ BOJ(2869).kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)) {
+    val a = nextInt()
+    val b = nextInt()
+    val v = nextInt()
+
+    print(if ((v-b)%(a-b) == 0) (v-a)/(a-b)+1 else (v-a)/(a-b)+2 )
+}
