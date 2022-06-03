75ab1aa83a0e90a9a4c844c00681141f1895c97e
SeonJK <tjswhdrbs14@naver.com>
2022-06-03 23:00:57 +0900

    Add title and runnable code

--- BOJ(1181).kt
+++ BOJ(1181).kt
@@ -1,21 +1,40 @@
+/* 단어 정렬 */
+
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val count = readLine().toInt()
-    val set = mutableSetOf<String>().apply {
-        repeat(count) {
-            this.add(readLine())
-        }
+    // 내가 푼 코드 -> 시간초과
+//    val count = readLine().toInt()
+//    val set = mutableSetOf<String>().apply {
+//        repeat(count) {
+//            this.add(readLine())
+//        }
+//    }
+//
+//    repeat(count) {
+//        if (set.isEmpty()){ return@with }
+//        val sortedList = set.sorted()
+//
+//        val min = sortedList.minByOrNull { it.length }!!
+//
+//        println(min)
+//        set.remove(min)
+//    }
+
+    // 다른 사람이 푼 코드
+    val n = readLine().toInt()
+    val set: MutableSet<String> = mutableSetOf()
+
+    repeat(n) {
+        set.add(readLine())
     }
 
-    repeat(count) {
-        if (set.isEmpty()){ return@with }
-        val sortedList = set.sorted()
+    val list = set.sortedWith(Comparator<String> { a, b ->
+        if (a.length > b.length) 1
+        else if (a.length < b.length) -1
+        else a.compareTo(b)
+    })
 
-        val min = sortedList.minByOrNull { it.length }!!
-
-        println(min)
-        set.remove(min)
-    }
+    list.forEach { println(it) }
 }
