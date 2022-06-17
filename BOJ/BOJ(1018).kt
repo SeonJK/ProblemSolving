1e3d3dc9826a8084b7c40a6b82eb6f01bcbc86b7
SeonJK <tjswhdrbs14@naver.com>
2022-06-17 21:33:11 +0900

    Add folder

---
+++ BOJ/BOJ(1018).kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    
+
+    // TODO: 흰색으로 시작할 때
+
+    // TODO: 검은색으로 시작할 때
+}
